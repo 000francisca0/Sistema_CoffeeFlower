e1360e8b70631804666c88da020823fc9e08bb64
000francisca0 <fr.sanhuezap@duocuc.cl>
2025-12-03 00:38:46 -0300

    Ahora sí, carpeta Ventas arreglada

---
+++ Ventas/src/main/java/Microservicio/Ventas/Repository/SaleRepository.java
@@ -0,0 +1,7 @@
+package Microservicio.Ventas.Repository;
+
+
+import Microservicio.Ventas.model.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> { }
